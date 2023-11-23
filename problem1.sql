db210d465071993baaee40c6ddc9b783e1afabc0
Artur Vyklynets <143753336+ArturVyklynets@users.noreply.github.com>
2023-11-23 20:44:19 +0200

    Update problem1.sql

--- problem1.sql
+++ problem1.sql
@@ -1,8 +1,9 @@
 -- Як можна планувати рейси для кожної станції на основі розкладів та часу прибуття/відправлення транспорту?
+```sql
 SELECT s.name_station, COUNT(*) AS number_of_departures
 FROM Stations s
 JOIN Schedules ON s.station_id = Schedules.station_id
 GROUP BY s.name_station
 ORDER BY number_of_departures DESC;
-
+```
 
